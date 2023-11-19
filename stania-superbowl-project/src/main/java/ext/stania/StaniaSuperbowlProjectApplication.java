4ba3c374fe9bce38d0735327a6967c4a7663606e
Jupas0 <hicham.keskas@gmail.com>
2023-11-19 12:37:33 +0100

    Initial Commit

---
+++ stania-superbowl-project/src/main/java/ext/stania/StaniaSuperbowlProjectApplication.java
@@ -0,0 +1,13 @@
+package ext.stania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaniaSuperbowlProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaniaSuperbowlProjectApplication.class, args);
+	}
+
+}
